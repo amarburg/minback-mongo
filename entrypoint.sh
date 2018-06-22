e1d8e860f5c769c6278aff5294431c3cf4aba39a
Aaron Marburg <amarburg@notetofutureself.org>
2018-06-22 11:06:35 -0700

    Add db name to archive title.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ ARGS="${@:2}"
 
 mc config host add mongodb "$MINIO_SERVER" "$MINIO_ACCESS_KEY" "$MINIO_SECRET_KEY" "$MINIO_API_VERSION" > /dev/null
 
-ARCHIVE="${MINIO_BUCKET}/${DB}-$(date $DATE_FORMAT).archive"
+ARCHIVE="${MINIO_BUCKET}/mongodump-$DB-$(date $DATE_FORMAT).archive"
 
 echo "Dumping $DB to $ARCHIVE"
 echo "> mongodump $ARGS -d $DB"
